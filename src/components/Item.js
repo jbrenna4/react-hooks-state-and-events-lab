c412bdea61cb18a4977eff56311b72c509f7c2bc
Jim Brennan <jim.j.brennan@gmail.com>
2023-02-05 14:14:03 -0500

    passed all the tests

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,18 +1,23 @@
+
 import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
 
-  const [inCart, setInCart] = useState(true);
-  function handleClick() {
-    setInCart((inCart) => !inCart);
-  
-} 
+  function handleAddToCartClick() {
+    setIsInCart((isInCart) => !isInCart);
+  }
 
   return (
-    <li className={inCart ? "" : "in-cart"}>
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add" onClick={handleClick}>{inCart ? "Add to cart" : "Remove from cart"}</button>
+      <button
+        className={isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick}
+      >
+        {isInCart ? "Remove From" : "Add to"} Cart
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -28,7 +28,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
